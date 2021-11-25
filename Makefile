sjp38/linux

(NOT-FOR-POSTING) DAMON hack tree commits start
Signed-off-by: SeongJae Park <sj@kernel.org>

Makefile
@@ -2,7 +2,7 @@
 VERSION = 5
 PATCHLEVEL = 15
 SUBLEVEL = 0
-EXTRAVERSION = -rc5-mm1
+EXTRAVERSION = -rc5-mm1-damon
 NAME = Opossums on Parade
 
 # *DOCUMENTATION*

kernelorg-mirror/linux_kernel_git_sj_linux

Makefile
@@ -2,7 +2,7 @@
 VERSION = 5
 PATCHLEVEL = 16
 SUBLEVEL = 0
-EXTRAVERSION = -rc2-mm1
+EXTRAVERSION = -rc2-mm1-damon
 NAME = Trick or Treat
 
 # *DOCUMENTATION*