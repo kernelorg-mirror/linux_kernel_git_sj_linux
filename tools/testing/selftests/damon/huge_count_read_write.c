d5150aa9378ccff545f5ceb5eb39d95fce13da42
SeongJae Park <sj@kernel.org>
2021-11-26 10:38:12 +0000

    selftests/damon: Test debugfs file ops with huge count
    DAMON debugfs interface users were able to trigger warning by writing
    some files with arbitrarily large 'count' parameter.  The issue is fixed
    with commit db7a347b26fe ("mm/damon/dbgfs: use '__GFP_NOWARN' for
    user-specified size buffer allocation").  This commit adds a test case
    for the issue in DAMON selftests to avoid future regressions.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

---
+++ tools/testing/selftests/damon/huge_count_read_write.c
@@ -0,0 +1,34 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+void test(char *file)
+{
+	int filedesc = open(file, O_RDWR);
+	char buf[25];
+	int ret;
+
+	printf("test %s\n", file);
+	if (filedesc < 0) {
+		fprintf(stderr, "failed opening %s\n", file);
+		exit(1);
+	}
+
+	write(filedesc, "", 0xfffffffful);
+	perror("after write: ");
+	ret = read(filedesc, buf, 0xfffffffful);
+	perror("after read: ");
+	close(filedesc);
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+		exit(1);
+	}
+	test(argv[1]);
+
+	return 0;
+}
