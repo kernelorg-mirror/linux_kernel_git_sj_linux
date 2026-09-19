daaae70b88244836531af1133ba2d09dd661b5ce
SJ Park <sj@kernel.org>
2026-09-19 15:22:26 -0700

    Revert "mm/damon/tests/vaddr-kunit: handle alloc failures in damon_test_split_evenly_fail()"
    This reverts commit 89532a71288cdcbfb6a6dfda331a939c87b08cec.
    
    Signed-off-by: SJ Park <sj@kernel.org>

--- mm/damon/tests/vaddr-kunit.h
+++ mm/damon/tests/vaddr-kunit.h
@@ -256,16 +256,7 @@ static void damon_test_split_evenly_fail(struct kunit *test,
 		unsigned long start, unsigned long end, unsigned int nr_pieces)
 {
 	struct damon_target *t = damon_new_target();
-	struct damon_region *r;
-
-	if (!t)
-		kunit_skip(test, "target alloc fail");
-
-	r = damon_new_region(start, end);
-	if (!r) {
-		damon_free_target(t);
-		kunit_skip(test, "region alloc fail");
-	}
+	struct damon_region *r = damon_new_region(start, end);
 
 	damon_add_region(r, t);
 	KUNIT_EXPECT_EQ(test,
