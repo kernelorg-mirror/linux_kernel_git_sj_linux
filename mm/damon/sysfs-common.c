intel-lab-lkp/linux

mm/damon/sysfs-schemes: move memcg_path_to_id() to sysfs-common
The next commit will need to find the memcg id from the user-passed path
to the memory cgroup, from sysfs.c.  memcg_path_to_id() is doing that, but
defined in sysfs-schemes.c as a static function.  Move the function to
sysfs-common.c and mark it as non-static, so that the next commit can
reuse the function.

Link: https://lore.kernel.org/20260518234119.97569-26-sj@kernel.org
Signed-off-by: SeongJae Park <sj@kernel.org>
Cc: David Hildenbrand <david@kernel.org>
Cc: Jonathan Corbet <corbet@lwn.net>
Cc: Liam R. Howlett <liam@infradead.org>
Cc: Lorenzo Stoakes <ljs@kernel.org>
Cc: "Masami Hiramatsu (Google)" <mhiramat@kernel.org>
Cc: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
Cc: Michal Hocko <mhocko@suse.com>
Cc: Mike Rapoport <rppt@kernel.org>
Cc: Shuah Khan <shuah@kernel.org>
Cc: Steven Rostedt <rostedt@goodmis.org>
Cc: Suren Baghdasaryan <surenb@google.com>
Cc: Vlastimil Babka <vbabka@kernel.org>
Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

mm/damon/sysfs-common.c
@@ -104,3 +104,44 @@ const struct kobj_type damon_sysfs_ul_range_ktype = {
 	.default_groups = damon_sysfs_ul_range_groups,
 };
 
+
+static bool damon_sysfs_memcg_path_eq(struct mem_cgroup *memcg,
+		char *memcg_path_buf, char *path)
+{
+#ifdef CONFIG_MEMCG
+	cgroup_path(memcg->css.cgroup, memcg_path_buf, PATH_MAX);
+	if (sysfs_streq(memcg_path_buf, path))
+		return true;
+#endif /* CONFIG_MEMCG */
+	return false;
+}
+
+int damon_sysfs_memcg_path_to_id(char *memcg_path, u64 *id)
+{
+	struct mem_cgroup *memcg;
+	char *path;
+	bool found = false;
+
+	if (!memcg_path)
+		return -EINVAL;
+
+	path = kmalloc_array(PATH_MAX, sizeof(*path), GFP_KERNEL);
+	if (!path)
+		return -ENOMEM;
+
+	for (memcg = mem_cgroup_iter(NULL, NULL, NULL); memcg;
+			memcg = mem_cgroup_iter(NULL, memcg, NULL)) {
+		/* skip offlined memcg */
+		if (!mem_cgroup_online(memcg))
+			continue;
+		if (damon_sysfs_memcg_path_eq(memcg, path, memcg_path)) {
+			*id = mem_cgroup_id(memcg);
+			found = true;
+			mem_cgroup_iter_break(NULL, memcg);
+			break;
+		}
+	}
+
+	kfree(path);
+	return found ? 0 : -EINVAL;
+}

kernelorg-mirror/linux_kernel_git_sj_linux

(cherry picked from commit b2025ce0662b186b3158c25f7f9c25b4e6931acc)
Signed-off-by: SJ Park <sj@kernel.org>

mm/damon/sysfs-common.c
@@ -105,3 +105,43 @@ const struct kobj_type damon_sysfs_ul_range_ktype = {
 	.default_groups = damon_sysfs_ul_range_groups,
 };
 
+static bool damon_sysfs_memcg_path_eq(struct mem_cgroup *memcg,
+               char *memcg_path_buf, char *path)
+{
+#ifdef CONFIG_MEMCG
+       cgroup_path(memcg->css.cgroup, memcg_path_buf, PATH_MAX);
+       if (sysfs_streq(memcg_path_buf, path))
+               return true;
+#endif /* CONFIG_MEMCG */
+       return false;
+}
+
+int damon_sysfs_memcg_path_to_id(char *memcg_path, unsigned short *id)
+{
+       struct mem_cgroup *memcg;
+       char *path;
+       bool found = false;
+
+       if (!memcg_path)
+               return -EINVAL;
+
+       path = kmalloc_array(PATH_MAX, sizeof(*path), GFP_KERNEL);
+       if (!path)
+               return -ENOMEM;
+
+       for (memcg = mem_cgroup_iter(NULL, NULL, NULL); memcg;
+                       memcg = mem_cgroup_iter(NULL, memcg, NULL)) {
+               /* skip removed memcg */
+               if (!mem_cgroup_id(memcg))
+                       continue;
+               if (damon_sysfs_memcg_path_eq(memcg, path, memcg_path)) {
+                       *id = mem_cgroup_id(memcg);
+                       found = true;
+                       mem_cgroup_iter_break(NULL, memcg);
+                       break;
+               }
+       }
+
+       kfree(path);
+       return found ? 0 : -EINVAL;
+}