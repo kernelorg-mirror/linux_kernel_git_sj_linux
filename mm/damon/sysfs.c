84645da52a743eec4ed8340c829ab2a507943644
SJ Park <sj@kernel.org>
2026-09-19 15:22:20 -0700

    Revert "mm/damon/sysfs: fix param_ctx leak on damon_sysfs_new_test_ctx() failure"
    This reverts commit e9de9f3ce06b133a348006668bc8d25c6e504867.
    
    Signed-off-by: SJ Park <sj@kernel.org>

--- mm/damon/sysfs.c
+++ mm/damon/sysfs.c
@@ -1476,10 +1476,8 @@ static int damon_sysfs_commit_input(void *data)
 	if (IS_ERR(param_ctx))
 		return PTR_ERR(param_ctx);
 	test_ctx = damon_new_ctx();
-	if (!test_ctx) {
-		damon_destroy_ctx(param_ctx);
+	if (!test_ctx)
 		return -ENOMEM;
-	}
 	err = damon_commit_ctx(test_ctx, param_ctx);
 	if (err)
 		goto out;
