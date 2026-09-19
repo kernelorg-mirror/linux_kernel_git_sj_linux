84e3896f49c2fc96c0814ebda8bc389fcc398b7e
SJ Park <sj@kernel.org>
2026-09-19 15:22:12 -0700

    Revert "samples/damon/wsse: stop and free damon ctx when damon_call() fails"
    This reverts commit cb55606449fd6d929be2d43face0f8e7b173acde.
    
    Signed-off-by: SJ Park <sj@kernel.org>

--- samples/damon/wsse.c
+++ samples/damon/wsse.c
@@ -92,12 +92,7 @@ static int damon_sample_wsse_start(void)
 		return err;
 	}
 	repeat_call_control.data = ctx;
-	err = damon_call(ctx, &repeat_call_control);
-	if (err) {
-		damon_stop(&ctx, 1);
-		damon_destroy_ctx(ctx);
-	}
-	return err;
+	return damon_call(ctx, &repeat_call_control);
 }
 
 static void damon_sample_wsse_stop(void)
